4f86e1570e1a11c5299b3d8f710fff474aa0f4d7
Mohammed Aljezawi <killerm250m@gmail.com>
2025-10-14 21:51:02 +0300

    Fix lint error: remove unnecessary import in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:dimelo_flutter/dimelo_flutter.dart';
 
 /// Dimelo Flutter Example App
